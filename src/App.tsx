2fa59b6b3eddf2f64a8232cd2d222c42f303e40c
huan.chen <huan.chen@mobilemd.cn>
2021-09-14 17:47:52 +0800

    x

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,15 @@ import React from 'react';
 import logo from './logo.svg';
 import './App.css';
 import {ProjectListScreen} from "./screens/project-list";
+import {LoginScreen} from "./screens/login";
 
 function App() {
-  return (
-    <div className="App">
-      <ProjectListScreen />
-    </div>
-  );
+    return (
+        <div className="App">
+            <LoginScreen/>
+            {/*<ProjectListScreen />*/}
+        </div>
+    );
 }
 
 export default App;
