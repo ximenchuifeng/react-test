be9048dd8bbaf72dc9945c80fdcae9beef2cf901
huan.chen <huan.chen@mobilemd.cn>
2021-09-17 17:18:21 +0800

    x

--- src/unauthenticated-app/login.tsx
+++ src/unauthenticated-app/login.tsx
@@ -1,5 +1,5 @@
 import {FormEvent} from "react";
-import {useAuth} from "../../context/auth-context";
+import {useAuth} from "../context/auth-context";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
@@ -14,9 +14,6 @@ export const LoginScreen = () => {
     }
 
     return <form onSubmit={handleSubmit}>
-        {
-            user ? <div>登录成功:用户名:{user?.name}</div> : null
-        }
         <div>
             <label htmlFor="username">用户名</label>
             <input type="text" id={'username'}/>
