4edb2ab64f64e5d1e13168afc89629040b644c7c
huan.chen <huan.chen@mobilemd.cn>
2021-09-17 16:10:30 +0800

    x

--- src/auth-provider.ts
+++ src/auth-provider.ts
@@ -6,15 +6,30 @@ const apiUrl = process.env.REACT_APP_API_URL;
 
 export const getToken = () => window.localStorage.getItem(localStorageKey)
 
-export const handleUserResponse = ({user}:{user:User}) => {
+export const handleUserResponse = ({user}: { user: User }) => {
     window.localStorage.setItem(localStorageKey, user.token || '')
     return user;
 }
 
 
+export const login = (data: { username: string, password: string }) => {
+    return fetch(`${apiUrl}/login`, {
+        method: 'POST',
+        headers: {
+            'Content-Type': 'application/json'
+        },
+        body: JSON.stringify(data)
+    }).then(async (response) => {
+        if (response.ok) {
+            return handleUserResponse(await response.json())
+        } else {
+            return Promise.reject(data)
+        }
+    })
+}
 
-export const login = (data: {username: string, password: string}) => {
-    fetch(`${apiUrl}/login`, {
+export const register = (data: { username: string, password: string }) => {
+    return fetch(`${apiUrl}/register`, {
         method: 'POST',
         headers: {
             'Content-Type': 'application/json'
@@ -23,22 +38,10 @@ export const login = (data: {username: string, password: string}) => {
     }).then(async response => {
         if (response.ok) {
             return handleUserResponse(await response.json())
+        } else {
+            return Promise.reject(data)
         }
     })
 }
 
-export const register = (data: {username: string, password: string}) => {
-    fetch(`${apiUrl}/register`, {
-        method: 'POST',
-        headers: {
-            'Content-Type': 'application/json'
-        },
-        body: JSON.stringify(data)
-    }).then(async response => {
-        if (response.ok) {
-            return handleUserResponse(await response.json())
-        }
-    })
-}
-
-export const logout = () => window.localStorage.removeItem(localStorageKey)
+export const logout = async () => window.localStorage.removeItem(localStorageKey)
--- src/screens/login/index.tsx
+++ src/screens/login/index.tsx
@@ -1,21 +1,10 @@
 import {FormEvent} from "react";
+import {useAuth} from "../../context/auth-context";
 
 const apiUrl = process.env.REACT_APP_API_URL;
 
 export const LoginScreen = () => {
-    const login = (param: { username: string, password: string }) => {
-        fetch(`${apiUrl}/login`, {
-            method: 'POST',
-            headers: {
-                'Content-Type': 'application/json'
-            },
-            body: JSON.stringify(param)
-        }).then(async response => {
-            if (response.ok) {
-
-            }
-        })
-    }
+    const {login, user} = useAuth()
 
     const handleSubmit = (event: FormEvent<HTMLFormElement>) => {
         event.preventDefault()
@@ -25,6 +14,9 @@ export const LoginScreen = () => {
     }
 
     return <form onSubmit={handleSubmit}>
+        {
+            user ? <div>登录成功:用户名:{user?.name}</div> : null
+        }
         <div>
             <label htmlFor="username">用户名</label>
             <input type="text" id={'username'}/>
